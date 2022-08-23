98654a718a6c7e52659deca42c887bc635a439d0
digambargiri27 <digambar.giri@gmail.com>
2022-08-23 11:57:53 +0000

    your commit message

--- regex02/http_parser.py
+++ regex02/http_parser.py
@@ -8,19 +8,23 @@ import requests
 import re
 
 def main():
-    # prompt user for "url" and "searchFor"
-    print(f"Welcome to the simple HTTP response parser. Where should we search (ex: https://alta3.com)?")
-    url = input()
-    print(f"Great! So we'll try to open this url {url} to search for the phrase:")
-    searchFor = input()
+    while True:
+        # prompt user for "url" and "searchFor"
+        print(f"Welcome to the simple HTTP response parser. Where should we search (ex: https://alta3.com)?")
+        url = input()
+        print(f"Great! So we'll try to open this url {url} to search for the phrase:")
+        searchFor = input()
 
-    # send an HTTP GET to the "url", then strip off the attached HTML data
-    searchMe = requests.get(url).text
+        # send an HTTP GET to the "url", then strip off the attached HTML data
+        searchMe = requests.get(url).text
 
-    if re.search(searchFor, searchMe):
-        print("Found a match!")
-    else:
-        print("No match!")
+        if re.search(searchFor, searchMe):
+            print("Found a match!")
+        else:
+            print("No match!")
+        str1 = input("Do you wnat to cont ? no / quit")
+        if str1 == 'quit':
+            break
 
 if __name__ == "__main__":
     main()
--- regex02/sipparser01.py
+++ regex02/sipparser01.py
@@ -15,21 +15,25 @@ def main():
     
     # searches test True if they find a match
     # False if no match
-    if conobj:
-        # display the match
-        print(conobj.group())
-        
-        # display the phone number (group 1)
-        print("The phone number is...")
-        print(conobj.group(1))
-        
-        # display the IPv6 (group 2)
-        print("The IPv6 is...")
-        print(conobj.group(2))
-        
-        # display the port (group 3)
-        print("The port is...")
-        print(conobj.group(3))
+    while True:
+        if conobj:
+            # display the match
+            print(conobj.group())
+            
+            # display the phone number (group 1)
+            print("The phone number is...")
+            print(conobj.group(1))
+            
+            # display the IPv6 (group 2)
+            print("The IPv6 is...")
+            print(conobj.group(2))
+            
+            # display the port (group 3)
+            print("The port is...")
+            print(conobj.group(3))
+        str = input()
+        if str == 'quit':
+            break
 
 if __name__ == "__main__":
     main()
